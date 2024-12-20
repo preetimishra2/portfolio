e4f7fa34c0c16077f15a578e5d59f504d5e2e634
preetimishra2 <preetimishra244@gmail.com>
2024-12-20 16:26:22 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <nav className="navbar">
         <div className="navbar-brand">Preeti Mishra</div>
         <ul className="nav-list">
-          <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
+          <li className="nav-item"><Link to="/portfolio" className="nav-link">Home</Link></li>
           <li className="nav-item"><Link to="/about" className="nav-link">About</Link></li>
           <li className="nav-item"><Link to="/experience" className="nav-link">Projects</Link></li>
           <li className="nav-item"><Link to="/contact" className="nav-link">Contact Me</Link></li>
