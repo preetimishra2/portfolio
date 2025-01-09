dcad7dfb4a621f581bdee871435948835c5252a0
preetimishra2 <preetimishra244@gmail.com>
2025-01-09 21:37:00 +0530

    making header more compatible for smaller devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header font-link">
       <nav className="navbar">
         <div className="navbar-brand">Preeti Mishra</div>
-        <ul className="nav-list">
-          <li className="nav-item"><Link to="/portfolio" className="nav-link">Home</Link></li>
-          <li className="nav-item"><Link to="/about" className="nav-link">About</Link></li>
-          <li className="nav-item"><Link to="/experience" className="nav-link">Projects</Link></li>
-          <li className="nav-item"><Link to="/contact" className="nav-link">Contact Me</Link></li>
+        <div className="menu-toggle" onClick={toggleMenu}>
+          {menuOpen ? '✖' : '☰'}
+        </div>
+        <ul className={`nav-list ${menuOpen ? 'active' : ''}`}>
+          <li className="nav-item">
+            <Link to="/portfolio" className="nav-link" onClick={() => setMenuOpen(false)}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link" onClick={() => setMenuOpen(false)}>
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/experience" className="nav-link" onClick={() => setMenuOpen(false)}>
+              Projects
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link" onClick={() => setMenuOpen(false)}>
+              Contact Me
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
